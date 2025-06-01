48710afeebcb6f5f1d714170deb7f3fcde897a9a
fami-gb <fami.gb0825@gmail.com>
2025-06-01 12:54:32 +0900

    first commit

---
+++ data.js
@@ -0,0 +1,46 @@
+export const data = [
+    {
+        name: "ETRobo GitHub",
+        url: "https://github.com/ETrobocon"
+    },
+    {
+        name: "ETRobo公式サイト",
+        url: "https://www.etrobo.jp/",
+        note: "規約や過去の大会の情報など"
+    },
+    {
+        name: "ETRobo Cybozu",
+        url: "https://etrobocon.cybozu.com/login",
+        note: "ID: etrobo_2025_0105<br>Pass: 2025kait<br>\
+                イベント資料などの配布場所"
+    },
+    {
+        name: "南関東ETRobo Youtube",
+        url: "https://www.youtube.com/@et5727"
+    },
+    {
+        name: "draw.io",
+        url: "https://app.diagrams.net/",
+        note: "モデリングツール"
+    },
+    {
+        name: "Git Mind",
+        url: "https://gitmind.com/app/docs/mbtmtudp",
+        note: "アイデア共有"
+    },
+    {
+        name: "Afrel",
+        url: "https://afrel.co.jp/product/spike/technology/",
+        note: "SPIKE Primeの技術情報"
+    },
+    {
+        name: "spike API(機体のAPI)",
+        url: "https://spike-rt.github.io/spike-rt/ja/html/modules.html",
+        note: "SPIKE APIのドキュメント"
+    },
+    {
+        name: "ev3 API",
+        url: "https://toppers.jp/ev3pf/EV3RT_CXX_API_Reference/",
+        note: "EV3 APIのドキュメント<br>C++"
+    }
+]
